9d32dac44e511e74c5e26e373b136325e6452dbf
TrEKi722 <105758761+TrEKi722@users.noreply.github.com>
2026-05-26 11:00:02 -0700

    Adding 2026 template

---
+++ config.js
@@ -0,0 +1,48 @@
+const CONFIG = {
+    font: "Sigher",
+};
+
+const TEMPLATES = {
+    "1": {
+        layers: ["template", "photo", "text"],
+        photo:       { cx: 450,  cy: 1378, w: 581, h: 697, angle: -18.2 },
+        quote:       { cx: 775,  cy: 515,  maxW: 1130, size: 56, weight: "normal", color: "white" },
+        attribution: { cx: 1091, cy: 910,  maxW: 625,  size: 65, weight: "normal", color: "white" },
+    },
+    "2": {
+        layers: ["template", "photo", "text"],
+        photo:       { cx: 1142, cy: 1462, w: 537, h: 648, angle: 20.4 },
+        quote:       { cx: 771,  cy: 534,  maxW: 1130, size: 56, weight: "normal", color: "white" },
+        attribution: { cx: 1092, cy: 911,  maxW: 625,  size: 65, weight: "normal", color: "white" },
+    },
+    "3": {
+        layers: ["template", "photo", "text"],
+        photo:       { cx: 437,  cy: 493,  w: 539, h: 643, angle: 15 },
+        quote:       { cx: 771,  cy: 1448, maxW: 1130, size: 56, weight: "normal", color: "white" },
+        attribution: { cx: 1091, cy: 1828, maxW: 625,  size: 65, weight: "normal", color: "white" },
+    },
+    "4": {
+        layers: ["template", "photo", "text"],
+        photo:       { cx: 1044, cy: 428,  w: 530, h: 645, angle: -26.5 },
+        quote:       { cx: 771,  cy: 1439, maxW: 1130, size: 56, weight: "normal", color: "white" },
+        attribution: { cx: 1092, cy: 1829, maxW: 625,  size: 65, weight: "normal", color: "white" },
+    },
+    "5": {
+        layers: ["template", "photo", "text"],
+        photo:       { cx: 401,  cy: 445,  w: 535, h: 645, angle: 14.6 },
+        quote:       { cx: 836,  cy: 1175, maxW: 1130, size: 56, weight: "normal", color: "white" },
+        attribution: { cx: 1155, cy: 1552, maxW: 625,  size: 65, weight: "normal", color: "white" },
+    },
+    "6": {
+        layers: ["template", "photo", "text"],
+        photo:       { cx: 1158, cy: 1447, w: 535, h: 645, angle: 20.6 },
+        quote:       { cx: 693,  cy: 795,  maxW: 1130, size: 56, weight: "normal", color: "white" },
+        attribution: { cx: 335,  cy: 1100, maxW: 625,  size: 65, weight: "normal", color: "white" },
+    },
+    "7": {
+        layers: ["photo", "text", "template"],
+        photo:       { cx: 1158, cy: 1447, w: 535, h: 645, angle: 20.6 },
+        quote:       { cx: 693,  cy: 795,  maxW: 1130, size: 56, weight: "normal", color: "white" },
+        attribution: { cx: 335,  cy: 1100, maxW: 625,  size: 65, weight: "normal", color: "white" },
+    },
+};
